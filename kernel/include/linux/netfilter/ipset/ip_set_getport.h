netgroup/srv6-pm-dp-ipset

commit 62d787ba5e66eb98c882f80dff49d0ea25c0e9db

netfilter: added missing includes to a number of header-files.
A number of netfilter header-files used declarations and definitions
from other headers without including them.  Added include directives to
make those declarations and definitions available.

Signed-off-by: Jeremy Sowden <jeremy@azazel.net>
Signed-off-by: Jozsef Kadlecsik <kadlec@netfilter.org>

kernel/include/linux/netfilter/ipset/ip_set_getport.h
@@ -2,6 +2,10 @@
 #ifndef _IP_SET_GETPORT_H
 #define _IP_SET_GETPORT_H
 
+#include <linux/skbuff.h>
+#include <linux/types.h>
+#include <uapi/linux/in.h>
+
 extern bool ip_set_get_ip4_port(const struct sk_buff *skb, bool src,
 				__be16 *port, u8 *proto);
 

serhepopovych/ipset

commit d31400351015aa8d90a52b2c2cc58618ea11ca20

ip_set: Include kernel header instead of UAPI
This header is used to build kernel modules not userspace thus it is
correct to include linux/in.h kernel variant and not UAPI.

This fixes build on old and not widely supported systems like RHEL6 and
Debian GNU/Linux 7 (wheezy) before headers split to UAPI and kernel.

Fixes: 62d787ba5e66 ("netfilter: added missing includes to a number of header-files.")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>
Signed-off-by: Jozsef Kadlecsik <kadlec@netfilter.org>

kernel/include/linux/netfilter/ipset/ip_set_getport.h
@@ -4,7 +4,7 @@
 
 #include <linux/skbuff.h>
 #include <linux/types.h>
-#include <uapi/linux/in.h>
+#include <linux/in.h>
 
 extern bool ip_set_get_ip4_port(const struct sk_buff *skb, bool src,
 				__be16 *port, u8 *proto);